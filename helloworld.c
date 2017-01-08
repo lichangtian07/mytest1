2917052a6f030874c152c427f057de7a024dabb6
lichangtian07 <li616007@126.com>
2017-01-08 23:40:38 +0800

    [info]: for all ,add test C file

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<string.h>
+
+
+int main(int argc, char ** argv)
+{
+	int a;
+	
+	printf("Hello , world !!!\n");
+	printf("Goodbye!!!  world !!!!\n");
+	
+	return 0;
+}
