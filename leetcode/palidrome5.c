0f67fd0a1792c495d930a2aca413fa065f1e5be4
lichangtian07 <li616007@126.com>
2019-03-28 00:12:49 +0800

    [info]: 修改Leetcode 第5题最长回文数

--- leetcode/palidrome5.c
+++ leetcode/palidrome5.c
@@ -5,46 +5,65 @@
 #include <string.h>
 
 
-char* longestPalindrome(char* s) {
+int centerExpend(char *s, int left, int right)
+{
+    int L = left;
+    int R = right;
+   
+    printf("1 ---------------------\n");
+    printf("s[%d] %c \n", L, s[L]);
+    printf("s[%d] %c \n", R, s[R]);
+
+    while( L >= 0 && R < strlen(s) && s[L] == s[R] )
+    {
+        L -- ;
+        R ++ ;
+        printf("s[%d] %c \n", L, s[L]);
+        printf("s[%d] %c \n", R, s[R]);
+    }
+
+    printf("2 ---------------------\n");
+    return  R - L - 1;
+}
+
+char* longestPalindrome(char* s) 
+{
     char    *pRet;              
-    int     len = 0, i = 0, k = 0, loc = 0; 
-    int     sublen = 0, maxlen = 1;    
-    char*   pTail;  
-    char*   pHead;                                                                                        
+    int     len = 0, i = 0;
+    int     start = 0 , end = 0;
+    int     loc = 0; 
+    int     maxlen = 1;    
+    int     lenOdd = 0;
+    int     lenEven = 0;
+    
     if(NULL == s)                             
     {   
         goto out;                                    
     } 
-    len = strlen(s);                         
+
+    len = strlen(s);
     if(len <= 1 )  
     {   
         maxlen = len;          
         goto out;                             
-    }                                                                                                                                                                      
+    }
+
     for(i = 0; i < len; i++)              
     {                            
-        for(k = len - 1; k > i; k--)     
-        {                                 
-            pHead = &s[i];                
-            pTail = &s[k];                 
-            sublen = ( k - i + 1)/2;      
-            while(sublen > 0)             
-            {                             
-                if(*pHead != *pTail)    
-                {                       
-                    break;             
-                }                                                                                                                             
-                pHead ++;                 
-                pTail --;                 
-                sublen --;               
-            }                                                                                                                                 
-            if(sublen == 0 && (maxlen < k - i + 1) ) 
-            {                     
-                loc = i;                   
-                maxlen = k - i + 1;       
-            }                                                                                                                                 
-        }                                                                                                                                  
-    }                                                                                               
+        lenOdd = centerExpend(s, i, i);
+        lenEven = centerExpend(s, i, i+1);
+        maxlen = lenOdd > lenEven ? lenOdd : lenEven ;
+
+        if( maxlen >  end -start)
+        {
+            start = i - (maxlen - 1) / 2;
+            end = i + maxlen / 2;
+        }
+
+    }
+    printf("start : %d \n",start);
+    printf("maxlen : %d \n",maxlen);
+    loc = start;
 
 out:
     pRet = (char *)malloc(maxlen * sizeof(char) + 1);  
@@ -55,86 +74,13 @@ out:
         return NULL;                                  
     }    
 
-    if(len > 0)
+    if(maxlen > 0)
     {
         memcpy(pRet, s + loc , maxlen);     
     }
 
     return pRet;
 }   
-
-/*
-   char* longestPalindrome(char* s) 
-   {
-   char *  pRet = NULL;
-   int     len = 0, i = 0, k = 0, loc = 0;
-   int     sublen = 0, maxlen = 1;
-   char*   pTail;
-   char*   pHead;
-
-   if(NULL == s)
-   {
-   goto out;
-   }
-
-   len = strlen(s);
-   if(len == 0 )
-   {
-   goto out; 
-   }
-
-   if(len == 1)
-   {
-   pRet = (char *)malloc(len * sizeof(char) + 1);
-   if(pRet == NULL)
-   {
-   printf("Error malloc ");
-   goto out;
-   }
-   memcpy(pRet, s, len);
-   }
-   else
-   {
-   for(i = 0; i < len; i++)
-   {
-   for(k = len - 1; k > i; k--)
-   {
-   pHead = &s[i];
-   pTail = &s[k];
-   sublen = ( k - i + 1)/2;
-   while(sublen > 0)
-   {
-   if(*pHead != *pTail)
-   {
-   break;
-   }
-   pHead ++;
-   pTail --;
-   sublen --;
-   }
-   if(sublen == 0 &&  maxlen < k - i + 1 )
-   {
-   loc = i;
-   maxlen = k - i + 1;
-   }
-   }
-
-   }
-
-   pRet = (char *)malloc(maxlen * sizeof(char) + 1);
-   if(pRet == NULL)
-   {
-   printf("Error malloc ");
-   goto out;
-   }
-   memcpy(pRet, s + loc , maxlen);
-   }
-
-out:
-return pRet;
-}
-*/
-
 
 int main(int argc, char **argv)
 {
